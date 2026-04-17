a6f47cc1fdec87107c098ab4f324e7f9f7c04c06
lab151-20 <faqihhidayah05@gmail.com>
2026-04-17 07:59:32 +0700

    Pertemuan 3 : penerapan intent

--- app/src/main/java/com/example/kipoapps/Pertemuan_4/FourthActivity.kt
+++ app/src/main/java/com/example/kipoapps/Pertemuan_4/FourthActivity.kt
@@ -1,4 +1,4 @@
-package com.example.kipoapps
+package com.example.kipoapps.Pertemuan_4
 
 import android.content.Intent
 import android.os.Bundle
@@ -7,29 +7,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.kipoapps.MainActivity
 import com.example.kipoapps.Pertemuan_3.ThirdResultActivity
-import com.example.kipoapps.Pertemuan_4.FourthActivity
-import com.example.kipoapps.databinding.ActivityMainBinding
+import com.example.kipoapps.R
+import com.example.kipoapps.databinding.ActivityFourthBinding
 import com.example.kipoapps.databinding.ActivityThirdBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class FourthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFourthBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityFourthBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding.btnSubmit.setOnClickListener {
-            val nomor = binding.inputText.text
+        binding.btnKirim.setOnClickListener {
+            val nomor = binding.inputTujuan.text
 
             Toast.makeText(this, "Nomor telah dikirm kan ke = $nomor", Toast.LENGTH_SHORT)
                 .show()
-            val intent = Intent(this, FourthActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
 
         }
